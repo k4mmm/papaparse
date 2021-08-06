eaaa68326c34121c50c625f87d2ffd16ef00ed3d
Kostia Chaikovskyi <chaichka175@gmail.com>
2021-08-06 09:27:27 +0300

    js correction

--- src/index.js
+++ src/index.js
@@ -55,7 +55,6 @@ function fetchData() {
                                 return value + "°С"
                             }
                         }
-                        // beginAtZero: true
                     }
                 }
             }
